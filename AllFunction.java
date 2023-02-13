a904b5148965410584d396e9e86856f19d30222a
Pawan Kumar Rajpott <pr5050296@gmail.com>
2023-02-13 12:25:10 -0500

    String commit

---
+++ AllFunction.java
@@ -0,0 +1,160 @@
+// public class AllFunction {
+//     public static void  main(String[] args) {
+//         String str = "Heaven Senpai";
+//         System.out.println(str.toLowerCase());
+//         System.out.println(str.toUpperCase());
+//         System.out.println(str.trim()); // remove extra spaces
+//         System.out.println(str.startsWith("H"));
+//         System.out.println(str.endsWith("i"));
+//         System.out.println(str.length());
+//         System.out.println(str.charAt(0));
+//         String s = String.valueOf(3);
+//         System.out.println(s);
+
+        
+//         String str2 = "Heaven Is Destiny To Be Great";
+//         System.out.println(str2);
+//         str2 = str2.replace("Great", "Greatest");
+//         System.out.println(str2);
+//         System.out.println(str.contains("ve"));
+//         String str3 = String.format("My Name is \"%s\"", "Suryansh");
+//         System.out.println(str3);
+//         String str4 = "Heaven Is Destiny To Be Great";
+//         System.out.printf("Quote : %s",str4);
+//         String str5 = "Naruto";
+//         byte[] bytes = str5.getBytes();
+//         for (int i = 0; i < bytes.length; i++) {
+//         System.out.println(bytes[i]);
+//         }
+//         String str6 = new String(bytes);
+//         System.out.println(str6);
+//         char[] ch = new char[10];// String str = "Heaven Senpai";
+//         System.out.println(str.toLowerCase());
+//         System.out.println(str.toUpperCase());
+//         System.out.println(str.trim()); // remove extra spaces
+//         System.out.println(str.startsWith("H"));
+//         System.out.println(str.endsWith("i"));
+//         System.out.println(str.length());
+//         System.out.println(str.charAt(0));
+//         String s = String.valueOf(3);
+//         System.out.println(s);
+//         String str2 = "Heaven Is Destiny To Be Great";
+//         System.out.println(str2);
+//         str2 = str2.replace("Great", "Greatest");
+//         System.out.println(str2);
+//         System.out.println(str.contains("ve"));
+//         String str3 = String.format("My Name is \"%s\"", "Suryansh");
+//         System.out.println(str3);
+//         String str4 = "Heaven Is Destiny To Be Great";
+//         System.out.printf("Quote : %s",str4);
+//         String str5 = "Naruto";
+//         byte[] bytes = str5.getBytes();
+//         for (int i = 0; i < bytes.length; i++) {
+//         System.out.println(bytes[i]);
+//         }
+//         String str6 = new String(bytes);
+//         System.out.println(str6);
+//         char[] ch = new cha// String str = "Heaven Senpai";
+//         System.out.println(str.toLowerCase());
+//         System.out.println(str.toUpperCase());
+//         System.out.println(str.trim()); // remove extra spaces
+//         System.out.println(str.startsWith("H"));
+//         System.out.println(str.endsWith("i"));
+//         System.out.println(str.length());
+//         System.out.println(str.charAt(0));
+//         String s = String.valueOf(3);
+//         System.out.println(s);
+//         String str2 = "Heaven Is Destiny To Be Great";
+//         System.out.println(str2);
+//         str2 = str2.replace("Great", "Greatest");
+//         System.out.println(str2);
+//         System.out.println(str.contains("ve"));
+//         String str3 = String.format("My Name is \"%s\"", "Suryansh");
+//         System.out.println(str3);
+//         String str4 = "Heaven Is Destiny To Be Great";
+//         System.out.printf("Quote : %s",str4);
+//         String str5 = "Naruto";
+//         byte[] bytes = str5.getBytes();
+//         for (int i = 0; i < bytes.length; i++) {
+//         System.out.println(bytes[i]);
+//         }
+//         String str6 = new String(bytes);
+//         System.out.println(str6);
+//         char[] ch = new char[10];
+//         str.getChars(2, 10, ch, 0);
+//         for (int i = 0; i < ch.length; i++) {
+//         System.out.println(ch[i]);
+//         }
+//         System.out.println(str.indexOf('n', 7));
+//         String str7 = new String("Heaven Senpai");
+//         String str8 = str.intern();
+//         System.out.println(str==str7);
+//         System.out.println(str==str8);
+//         System.out.println(str.isEmpty());
+//         String str9 = String.join(":", "00","10","15");
+//         System.out.println(str9);
+//         System.out.println(str.lastIndexOf("S"));
+//         str = str.replace("Heaven", "Sasuke");
+//             String str10 = new String("Apple1202312Apple");
+//             str10 = str10.replaceAll("\\d","Z");
+//             // System.out.println(str10);
+//             String str11 = "Sakura Is Worst";
+//             String[] arr = str11.split(" ",2);
+//             for (String s : arr)  {
+//                 System.out.println(s);
+//             }
+//             int value = 10;
+//             String name = new String(String.valueOf(value));
+//             System.out.println(name+0+1);r[10];
+//         str.getChars(2, 10, ch, 0);
+//         for (int i = 0; i < ch.length; i++) {
+//         System.out.println(ch[i]);
+//         }
+//         System.out.println(str.indexOf('n', 7));
+//         String str7 = new String("Heaven Senpai");
+//         String str8 = str.intern();
+//         System.out.println(str==str7);
+//         System.out.println(str==str8);
+//         System.out.println(str.isEmpty());
+//         String str9 = String.join(":", "00","10","15");
+//         System.out.println(str9);
+//         System.out.println(str.lastIndexOf("S"));
+//         str = str.replace("Heaven", "Sasuke");
+//             String str10 = new String("Apple1202312Apple");
+//             str10 = str10.replaceAll("\\d","Z");
+//             // System.out.println(str10);
+//             String str11 = "Sakura Is Worst";
+//             String[] arr = str11.split(" ",2);
+//             for (String s : arr)  {
+//                 System.out.println(s);
+//             }
+//             int value = 10;
+//             String name = new String(String.valueOf(value));
+//             System.out.println(name+0+1);
+//         str.getChars(2, 10, ch, 0);
+//         for (int i = 0; i < ch.length; i++) {
+//         System.out.println(ch[i]);
+//         }
+//         System.out.println(str.indexOf('n', 7));
+//         String str7 = new String("Heaven Senpai");
+//         String str8 = str.intern();
+//         System.out.println(str==str7);
+//         System.out.println(str==str8);
+//         System.out.println(str.isEmpty());
+//         String str9 = String.join(":", "00","10","15");
+//         System.out.println(str9);
+//         System.out.println(str.lastIndexOf("S"));
+//         str = str.replace("Heaven", "Sasuke");
+//             String str10 = new String("Apple1202312Apple");
+//             str10 = str10.replaceAll("\\d","Z");
+//             // System.out.println(str10);
+//             String str11 = "Sakura Is Worst";
+//             String[] arr = str11.split(" ",2);
+//             for (String s : arr)  {
+//                 System.out.println(s);
+//             }
+//             int value = 10;
+//             String name = new String(String.valueOf(value));
+//             System.out.println(name+0+1); 
+//     }
+}
